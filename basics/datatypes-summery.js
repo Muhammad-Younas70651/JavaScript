764be4131afd1cc5ae47f0e50aee8dd091eb5928
Muhammad-Younas70651 <myounas8776@gmail.com>
2025-12-01 06:03:07 +0000

    datatype-summery

---
+++ basics/datatypes-summery.js
@@ -0,0 +1,24 @@
+// Primitive datatypes 
+// its has 7 types : string ,Number , boolean , null , undefined , symbol and BigInt
+
+
+// js is dynamically typed langugae cux we dont define a veribale in this 
+// eg
+// const score = 100; 
+
+
+// Refrence (non primitive data type )\
+// its has 3 types : Array , objects and functions
+
+
+const heros = ["abc", "def", "ghi"];
+
+let obj{
+    age = 22,
+    name = "Muhammad Younas "
+}
+
+const myfunction = function(){
+    console.log("Hello worlds ");
+    
+}
