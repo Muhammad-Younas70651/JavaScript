5f2d8cb8eac70a97a7546c598534395fd47931b1
Muhammad-Younas70651 <myounas8776@gmail.com>
2025-12-01 09:45:49 +0000

    add nums-and-maths.js with number and math operations examples

---
+++ basics/nums-and-maths.js
@@ -0,0 +1,29 @@
+// Numbers 
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString() );
+// console.log(balance.toFixed(2) );
+
+const otherNumber = 123.87;
+// console.log(otherNumber.toPrecision(3));
+
+// Maths 
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.min( 1 , 2, 4 , 6,65 ));
+// console.log(Math.max( 1 , 2, 4 , 6,65 ));
+
+console.log(Math.random());// its always give us the value between 0 and 1 
+console.log(Math.random()*10 +1 ); // Thats why we use this value to get a greater value form 0 
+console.log((Math.random()*10 ) +1 );
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
