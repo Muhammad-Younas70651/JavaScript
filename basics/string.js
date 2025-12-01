f6eb70f44bdb1d2dcb560292447f2fabfa5eadc6
Muhammad-Younas70651 <myounas8776@gmail.com>
2025-12-01 09:34:10 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,6 @@
+const name = "Muhammad Younas"
+const repoCount = 50
+
+// console.log (name + repoCount);
+console.log(`My name is ${name} my Repositre count is ${repoCount}`);// this method is called string interpullation
+
