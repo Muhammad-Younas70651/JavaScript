8f1617e5251f9af35fb898818421d67936a7356d
Muhammad-Younas70651 <myounas8776@gmail.com>
2025-12-01 05:37:13 +0000

    comparition

---
+++ basics/comparision.js
@@ -0,0 +1,14 @@
+// console.log( 2 > 1);
+// console.log (1 >= 2);
+// console.log ( 1 == 1);
+// console.log (1 != 2);
+
+// console.log ("2" >1);
+// console.log ("02" >1 );
+
+console.log(null > 0);
+console.log(null <= 0);
+console.log( null == 0);
+
+
+// when we use === its start strick checking menas check the value and also check the datatype and when we use == so its check only the values in the program 
