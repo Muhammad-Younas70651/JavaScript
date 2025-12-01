b8fe86653b412f72fb972a6166988ac5b814c33a
Muhammad-Younas70651 <myounas8776@gmail.com>
2025-12-01 15:51:05 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,13 @@
+// Dates 
+
+const mydate = new Date();
+// console.log(mydate.toString()); 
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+
+// let myCreateDtae = new Date (2025 ,11 , 1);
+// let myCreateDtae = new Date (2025 ,11 , 1, 5 ,3);
+let myCreateDtae = new Date ("01-11-2025");
+console.log(myCreateDtae.toLocaleString());
